fe6b9fbb590dcad164bafc0cc295058b2a06113b
KubikZ <jakszewczyk@student.agh.edu.pl>
2025-12-02 14:21:35 +0100

    Refactor and bug fixes for lab3

--- lab3/compress.py
+++ lab3/compress.py
@@ -2,6 +2,7 @@ from scipy.linalg import svd
 import os
 import numpy as np
 from PIL import Image, ImageDraw
+import matplotlib.pyplot as plt
 
 eps = 1e-6
 
@@ -13,10 +14,15 @@ class MatrixLeaf:
             self.is_zero = True
             return
         self.is_zero = False
-        self.rank = r
-        self.singular_vals = D[:r]
-        self.U = U[:, :r]
-        self.V = V[:r, :]
+
+        rows, cols = A.shape
+        max_possible_rank = min(rows, cols)
+        available_singular_values = len(D)
+        self.rank = min(r, max_possible_rank, available_singular_values)
+
+        self.singular_vals = D[:self.rank]
+        self.U = U[:, :self.rank]
+        self.V = V[:self.rank, :]
         
 
 class MatrixNode:
@@ -29,7 +35,7 @@ def CreateTree(A, t_min, t_max, s_min, s_max, r, epsilon):
     U, D, V = svd(A_part)
     node: MatrixNode
     D_epsilon = D[np.where(D > epsilon)]
-    if len(D_epsilon) <= r and len(D_epsilon) <= len(A_part):
+    if len(D_epsilon) <= r:
         node = MatrixLeaf(A_part, (t_min, t_max, s_min, s_max), U, D, V, r)
     else:
         node = MatrixNode()
@@ -61,6 +67,46 @@ def ReconstructMatrix(tree_node, shape):
     reconstruct_node(tree_node, result)
     return result
 
+def calculate_compression_stats(tree_root, original_shape, original_dtype=np.float64):
+    def count_parameters(node):
+        if isinstance(node, MatrixLeaf):
+            if node.is_zero or node.rank == 0:
+                return 0
+            else:
+                t_min, t_max, s_min, s_max = node.size
+                rows = t_max - t_min + 1
+                cols = s_max - s_min + 1
+                r = node.rank
+                return (rows * r) + r + (r * cols)
+        else:
+            total = 0
+            for child in node.children:
+                total += count_parameters(child)
+            return total
+    
+    original_rows, original_cols = original_shape
+    original_elements = original_rows * original_cols
+    original_bytes = original_elements * np.dtype(original_dtype).itemsize
+    
+    compressed_elements = count_parameters(tree_root)
+    
+    compression_ratio = original_elements / compressed_elements if compressed_elements > 0 else float('inf')
+    compression_percentage = (1 - (compressed_elements / original_elements)) * 100
+    
+    compressed_bytes = compressed_elements * np.dtype(np.float64).itemsize
+    
+    return {
+        'original_elements': original_elements,
+        'compressed_elements': compressed_elements,
+        'compression_ratio': compression_ratio,
+        'compression_percentage': compression_percentage,
+        'space_saving_percentage': compression_percentage,
+        'original_bytes': original_bytes,
+        'compressed_bytes': compressed_bytes,
+        'bytes_saved': original_bytes - compressed_bytes,
+        'bits_per_element': (compressed_bytes * 8) / original_elements if original_elements > 0 else 0
+    }
+
 
 def draw_box(draw, x1, y1, x2, y2):
     draw.line((x1, y1, x2, y1), fill=128)
@@ -80,31 +126,3 @@ def draw_vis(matrix_root):
     result = ImageDraw.Draw(im)
     draw_lines(result, matrix_root)
     return im
-
-
-# from scipy.linalg import svd
-# from compress import CompressMatrix, draw_vis
-
-
-# IMAGEFILE = 'img.bmp'
-
-# image = Image.open(IMAGEFILE)
-# im_rgb = np.array(image)
-
-# R = im_rgb[:,:,0]
-# G = im_rgb[:,:,1]
-# B = im_rgb[:,:,2]
-
-# _, D, _ = svd(R)
-# # print(D)
-# R_compressed = CompressMatrix(R, 1, D[0])
-# G_compressed = CompressMatrix(G, 1, D[0])
-# B_compressed = CompressMatrix(B, 1, D[0])
-
-# R_im = get_color_channel(image, 0)
-# draw_vis(R_compressed, R_im)
-# # B_im = draw_vis(G_compressed, image)
-# # G_im = draw_vis(B_compressed, image)
-
-
-
